17108eb244e88368b20fd6cca6de8c184fb46b1f
Octavian-Nicolae Doncea <octaviandoncea@gmail.com>
2026-05-13 12:00:35 +0300

    Delete backend/app/routers/schemas.py

--- backend/app/routers/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class SessionOut(BaseModel):
-    id: int
-    name: str
-    created_at: datetime
-
-    model_config = {'from_attributes': True}
